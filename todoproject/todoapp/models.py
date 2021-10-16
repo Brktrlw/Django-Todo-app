30bc0bacf4e4b22c0fc59e6520feaffc5ebdb31a
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-16 10:35:37 +0300

    Update

--- todoproject/todoapp/models.py
+++ todoproject/todoapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
+class Todo(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField(max_length=500,blank=True)
+    isFinished=models.BooleanField(default=True)
+    date=models.DateTimeField(default=datetime.now())
 
-# Create your models here.
+print("a")
