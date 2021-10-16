5369fee98e133062d8eae9f8a651689c2f0c6500
BrktrL <74716514+Brktrlw@users.noreply.github.com>
2021-10-16 11:59:12 +0300

    Update

--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo
+from .forms import TodoForm
 def index(request):
     return render(request,"todoapp/index.html")
 
@@ -8,7 +9,14 @@ def about(request):
     return render(request,"todoapp/about.html")
 
 def create(request):
-    return render(request,"todoapp/createtodo.html")
+    if request.method=="POST":
+        form=TodoForm(request.POST or None)
+        if form.is_valid:
+            form.save()
+            return render(request, "todoapp/createtodo.html")
+
+    else:
+        return render(request,"todoapp/createtodo.html")
 
 def listeler(request):
     todo_list=Todo.objects.all()
